4126c28be6d6418d4152732bf88070635383c116
Michael Wender <michael@michaelwender.com>
2024-02-02 14:52:45 -0500

    Adding ACF_PRO_LICENSE to available .env vars

--- config/application.php
+++ config/application.php
@@ -111,6 +111,7 @@ Config::define('NONCE_SALT', env('NONCE_SALT'));
  */
 Config::define('AUTOMATIC_UPDATER_DISABLED', true);
 Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
+Config::define('ACF_PRO_LICENSE', env('ACF_PRO_LICENSE') ?: false);
 
 // Disable the plugin and theme file editor in the admin
 Config::define('DISALLOW_FILE_EDIT', true);
